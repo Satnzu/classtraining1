58501d5ea71d5c615909d14d5ef10a12282693cd
santeri tammivuori <49670127+Satnzu@users.noreply.github.com>
2019-04-17 08:30:39 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class kissa
+    {
+        public string nimi;
+        private int ika;
+        public kissa()
+        {
+            nimi = "";
+            ika = 0;
+        }
+
+        public string Nimi { get; set; }
+
+        public int Ika { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kissa cat = new kissa();
+            Console.WriteLine(cat.nimi);
+            cat.nimi = "tuukka";
+            Console.WriteLine(cat.nimi + cat.Ika);
+            kissa cat2 = new kissa();
+            Console.WriteLine(cat2.nimi + cat2.Ika);
+            Console.ReadKey();
+        }
+    }
+}
